0e97a5e3e2cd6c9a627aa2b43c182f59e2e950a4
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-08 22:08:51 +0100

    Headphone Inspiration

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let mobileMenu = document.getElementById("mobileMenu")
+let mobileMenuList = document.getElementById("mobileMenuList")
+
+function toggleMenu(){
+    mobileMenu.classList.toggle("toggle")
+    mobileMenuList.classList.toggle("hide")
+}
